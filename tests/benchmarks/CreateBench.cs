8ee505b7d9036acdc8552bb21c64a8e03425c049
malstraem <malstraem@gmail.com>
2025-11-28 19:37:59 +0300

    update packages, dotnet and CI

--- tests/benchmarks/CreateBench.cs
+++ tests/benchmarks/CreateBench.cs
@@ -1,7 +1,9 @@
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
 
 namespace Arinc424.Bench;
 
+[SimpleJob(RuntimeMoniker.Net10_0)]
 public class CreateBench
 {
     private readonly string[] strings = File.ReadAllLines($"{Cases}worldwide");
@@ -9,7 +11,7 @@ public class CreateBench
     private readonly Meta424 meta = Meta424.Create(Supplement.V18);
 
     [Benchmark(Description = "Create (worldwide data 100 mb)")]
-    public Data424 Create() => Data424.Create(meta, strings, out var _, out var _);
+    public Data424 Create() => Data424.Create(meta, strings, out string[] _, out var _);
 
     [Benchmark(Description = "Compile metadata")]
     public Meta424 CreateMeta() => Meta424.Create(Supplement.V23);
