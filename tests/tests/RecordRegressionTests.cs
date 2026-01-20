44add9712a8cf7d6b8d949452567f8e5e21f6c4e
malstraem <malstraem@gmail.com>
2026-01-20 19:18:49 +0300

    up

--- tests/tests/RecordRegressionTests.cs
+++ tests/tests/RecordRegressionTests.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Frozen;
 using System.Text.Json;
 
 namespace Arinc424.Tests;
@@ -27,9 +26,7 @@ public class RecordRegressionTests : BaseTests
         foreach (var property in typeof(Data424).GetProperties().Where(x => x.PropertyType.IsArray))
             counts.Add(property.Name, ((Array)property.GetValue(data)!).Length);
 
-        Regression regression = new() { Invalid = invalid.ToFrozenDictionary(), Counts = counts.ToFrozenDictionary() };
-
-        string biba = JsonSerializer.Serialize(regression, options);
+        Regression regression = new() { Invalid = invalid, Counts = counts };
 
         File.WriteAllText($"{Regressions}{file}.json", JsonSerializer.Serialize(regression, options));
     }
@@ -38,20 +35,29 @@ public class RecordRegressionTests : BaseTests
     [Arguments("worldwide", Supplement.V18)]
     [Arguments("faa-07.08.25", Supplement.V18)]
     [Arguments("supplement-18", Supplement.V18)]
-    public void CheckRegression(string file, Supplement supplement)
+    public async Task CheckRegression(string file, Supplement supplement)
     {
+        var regression = JsonSerializer.Deserialize<Regression>(File.ReadAllText($"{Regressions}{file}.json"), options)!;
+
         string[] strings = File.ReadAllLines($"{Cases}{file}");
 
-        var data = Data424.Create(meta[supplement], strings, out string[] _, out var _);
+        var data = Data424.Create(meta[supplement], strings, out _, out var invalid);
 
-        var counts = JsonSerializer.Deserialize<Dictionary<string, int>>(File.ReadAllText($"{Regressions}{file}.json"))!;
-
-        foreach (var (propertyName, expected) in counts)
+        foreach (var (propertyName, expected) in regression.Counts)
         {
             int count = ((ICollection)typeof(Data424).GetProperty(propertyName)!.GetValue(data)!).Count;
 
             if (count != expected)
                 Assert.Fail($"'{propertyName}' regression - expected: {expected}, actual: {count}");
+        }
+
+        foreach (var (record, diagnostics) in invalid)
+        {
+            if (!regression.Invalid.TryGetValue(record.Source!, out string[]? expected))
+                Assert.Fail($"'{file}' regression - source '{record.Source!}' is not found.");
+
+            if (diagnostics.Length != expected.Length)
+                Assert.Fail($"'{file}' regression - diagnsotics count is not equal to previos.");
         }
     }
 }
--- tests/tests/Regression.cs
+++ tests/tests/Regression.cs
@@ -1,10 +1,8 @@
-using System.Collections.Frozen;
-
 namespace Arinc424.Tests;
 
 public class Regression
 {
-    public required FrozenDictionary<string, int> Counts { get; set; }
+    public required Dictionary<string, int> Counts { get; set; }
 
-    public required FrozenDictionary<string, string[]> Invalid { get; set; }
+    public required Dictionary<string, string[]> Invalid { get; set; }
 }
